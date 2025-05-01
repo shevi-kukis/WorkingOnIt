b9ac5e3b479eeae0d37909ab7ec3656a8ed4b99a
shevi <shevi3151@gmail.com>
2025-05-02 01:39:51 +0300

    render

--- workingOnItClientReact/workingOnIt/src/components/axiosInstance.ts
+++ workingOnItClientReact/workingOnIt/src/components/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://localhost:7288/api",
+  baseURL: "https://localhost:32769/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
